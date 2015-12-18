8cdb2c046e58763c37434e6eff8cd2454be5c79f
MetalDestructor <rapid3fighter@hotmail.com>
2015-12-18 15:43:22 +0200

    Minor Updates

--- 10-2015-Java-Fundamentals/06. Loops/Problem06/Calculate.java
+++ 10-2015-Java-Fundamentals/06. Loops/Problem06/Calculate.java
@@ -3,26 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Calculate {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(reader.readLine());
-		int x = Integer.parseInt(reader.readLine());
+		int k = Integer.parseInt(reader.readLine());
 
-		double sum = 1;
-		for (int i = 1; i <= n; i++) {
-			sum += fact(i) / Math.pow(x, i);
-		}
-
-		System.out.printf("%.5f", sum);
+		int result = fact(n) / fact(k);
+		System.out.println(result);
 	}
 
-	public static int fact(int num) {
+	public static int fact(int n) {
 		int fact = 1;
-		for (int i = 1; i <= num; i++) {
+		for (int i = 1; i <= n; i++) {
 			fact *= i;
 		}
 		return fact;
 	}
-
 }
--- 10-2015-Java-Fundamentals/06. Loops/Problem07/Calculate.java
+++ 10-2015-Java-Fundamentals/06. Loops/Problem07/Calculate.java
@@ -7,18 +7,14 @@ public class Calculate {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(reader.readLine());
-		int x = Integer.parseInt(reader.readLine());
-
-		double sum = 1;
-		for (int i = 1; i <= n; i++) {
-			sum += fact(i) / Math.pow(x, i);
-		}
-
-		System.out.printf("%.5f", sum);
+		int k = Integer.parseInt(reader.readLine());
+		
+		long result = (fact(n))/(fact(k) * (fact(n-k)));
+		System.out.println(result);
 	}
 
-	public static int fact(int num) {
-		int fact = 1;
+	public static long fact(int num) {
+		long fact = 1;
 		for (int i = 1; i <= num; i++) {
 			fact *= i;
 		}
