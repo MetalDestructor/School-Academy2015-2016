370199a3fcb47fee8a6b266c3534702f02abb4da
MetalDestructor <rapid3fighter@hotmail.com>
2015-12-21 22:52:49 +0200

    Added 1 task cuz exhausted

---
+++ 10-2015-Java-Fundamentals/06. Loops/Problem17/CalculateGCD.java
@@ -0,0 +1,9 @@
+
+public class CalculateGCD {
+
+	public static void main(String[] args) {
+		// TODO
+
+	}
+
+}
---
+++ 10-2015-Java-Fundamentals/06. Loops/Problem18/TrailingZeroesInNFact.java
@@ -0,0 +1,9 @@
+
+public class TrailingZeroesInNFact {
+
+	public static void main(String[] args) {
+		// TODO
+
+	}
+
+}
