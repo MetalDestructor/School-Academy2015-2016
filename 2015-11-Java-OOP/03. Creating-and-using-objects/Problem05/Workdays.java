8c665a431fc2ab4c76d2d7d4ca4a1caff6b20f41
MetalDestructor <rapid3fighter@hotmail.com>
2015-12-28 21:47:54 +0200

    For the funs and giggles

---
+++ 2015-11-Java-OOP/03. Creating-and-using-objects/Problem05/Workdays.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Workdays {
+
+	public static void main(String[] args) {
+		Date[] dates = {
+				new Date(),
+				new Date(2007,05,06),
+				new Date(2007,05,06),
+				new Date(2007,05,06),
+		};
+		System.out.println(dates[0]);
+	}
+
+}
